ff5d6fb3b14e02f0a2d396c7a99a9a875cc1f8f4
Taylor Pospisil <tpospisi@andrew.cmu.edu>
2018-03-01 16:51:37 -0500

    Add example usage test

---
+++ tests/testthat/test-example.R
@@ -0,0 +1,38 @@
+context("Test example")
+
+test_that("Sample usage case works", {
+  set.seed(42)
+
+  n_train <- 1000
+  n_validation <- 1000
+  n_test <- 100
+  n_grid <- 100
+
+  z_grid <- seq(0, 1, length.out = n_grid)
+
+  x_train <- runif(n_train)
+  z_train <- runif(n_train, 0, x_train)
+
+  x_validation <- runif(n_validation)
+  z_validation <- runif(n_validation, 0, x_validation)
+
+  x_test <- runif(n_test)
+  z_test <- runif(n_test, 0, x_test)
+
+  ## Train
+  obj <- NNKCDE$new(x_train, z_train)
+
+  ## Estimate errors
+  k_grid <- 1:10
+  loss_df <- obj$estimate_loss(x_validation, z_validation, k_grid = k_grid)
+  expect_equal(nrow(loss_df), 10)
+  expect_true(all(k_grid %in% loss_df$k))
+
+  ## Tune to minimum loss
+  obj$tune(x_validation, z_validation, k_grid = seq(1, 20, 100), h = 0.05)
+  expect_false(is.null(obj$k))
+  expect_false(is.null(obj$h))
+
+  cdes <- obj$predict(x_test, z_grid)
+  expect_equal(dim(cdes), c(n_test, n_grid))
+})
